27b1564d0d45d1565f3305e04ef01e9093421e1a
unknown <nbansal1582004@gmail.com>
2026-03-17 11:55:59 +0530

    backend intialise

---
+++ backend/4_math.js
@@ -0,0 +1,20 @@
+export function add(a,b){
+    return a+b;
+
+}
+export function sub(a,b){
+    return a-b;
+}
+
+console.log(add(4,5))
+function myName(){
+    console.log("n");
+     console.log("i");
+      console.log("t");
+       console.log("t");
+        console.log("s");
+         console.log("h");
+    
+}
+
+console.log(myName());
---
+++ backend/main.js
@@ -0,0 +1,5 @@
+import { add,sub } from "./4_math.js";
+console.log(add(4,6));
+console.log(sub(5,8));
+
+
